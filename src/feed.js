a8a040210d877c0f05e6580a2fe5d81f21ee82ad
timbt <trbeattie@outlook.com>
2014-10-16 22:40:40 -0400

    stuff

--- src/feed.js
+++ src/feed.js
@@ -1,17 +1,19 @@
-var http = require("http");
-var eventEmitter = require("events").EventEmitter;
-var feedParser = require("feedparser");
+//feed.js -- part of airzreader by Tim Beattie (github.com/timbt).
 
-module.exports = new eventEmitter();
+var http = require("http"); //Used to get RSS feed
+var eventEmitter = require("events").EventEmitter; //Idk this makes javascript stuff async stuff work
+var feedParser = require("feedparser"); 
+
+module.exports = new eventEmitter(); //Wrapping the export module as an event emitter allows asynchronous execution in other scripts once this script has successfully loaded and parsed the feed.
 
 var update = function () {http.get("http://www.reddit.com/user/airz23/submitted/.rss", function(response){
 	
-	var feedMeta;
-	var stories = [];
+	var feedMeta; //Store metadata. Just in case y'know.
+	var stories = []; //Main array to hold loaded articles.
 
 	response.pipe(new feedParser({}))
 		.on('error', function(error){
-			console.log("error in feed.js");
+			console.log("error in feed.js"); //Log if feed load unsuccessful.
 		})
 		.on('meta', function(meta){
 			feedMeta = meta;
@@ -29,10 +31,10 @@ var update = function () {http.get("http://www.reddit.com/user/airz23/submitted/
 				module.exports.meta = feedMeta;
 				module.exports.stories = stories;
 				module.exports.emit('ready');
-				console.log("Hello from feed.js: " + stories.length); //Testing
+				//console.log("Hello from feed.js: " + stories.length); //Used for testing
 		})
 
-})};
+})}; //Async is messy
 
 update(); //Initialize on server start-up
 setInterval(update,1000 * 60 * 5); //Every 5 minutes
